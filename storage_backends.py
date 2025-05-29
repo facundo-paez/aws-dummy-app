a1997d969f1627523e5d9a8ed23a45d78e679e4d
Facundo Paez <f.fpaez97@gmail.com>
2025-05-29 20:27:13 -0300

    Fixed decouple error.

--- storage_backends.py
+++ storage_backends.py
@@ -1,7 +1,8 @@
 from storages.backends.s3boto3 import S3Boto3Storage
-from decouple import config
+from dummy_app.settings import get_param
+# from decouple import config
 
 class StaticStorage(S3Boto3Storage):
-    location = config('AWS_STATIC_LOCATION', default='static')
+    location = get_param('STATIC_LOCATION')
     default_acl = 'public-read'
-    custom_domain = f"{config('AWS_STORAGE_BUCKET_NAME')}.s3.amazonaws.com"
+    custom_domain = f"{get_param('STORAGE_BUCKET_NAME')}.s3.amazonaws.com"
